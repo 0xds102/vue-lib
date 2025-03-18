f47d55723a0485958a34722a6917acef47c82284
Darin Shaffer <dandrew@hrworkcycles.com>
2025-03-18 13:27:14 -0500

    feat(Label): created label component

---
+++ src/components/ui/label/Label.vue
@@ -0,0 +1,24 @@
+<template>
+    <label
+      :for="htmlFor"
+      :class="[
+        'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+        'text-base-content',
+        className
+      ]"
+    >
+      <slot></slot>
+    </label>
+  </template>
+  
+  <script setup lang="ts">
+  interface Props {
+    htmlFor?: string
+    className?: string
+  }
+  
+  withDefaults(defineProps<Props>(), {
+    htmlFor: undefined,
+    className: ''
+  })
+  </script>
